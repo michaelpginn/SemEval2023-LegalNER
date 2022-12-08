d52632085897732f2d17a1633575755b9220f2a7
michaelpginn <milespergallon271@gmail.com>
2022-12-08 10:18:06 -0700

    Add eval script

---
+++ eval.py
@@ -0,0 +1,87 @@
+def load_data(spacy_file='training/data/train.spacy'):
+    print("Loading data...")
+    doc_bin = DocBin().from_disk(spacy_file)
+    nlp = spacy.load('en_core_web_trf')
+    docs = doc_bin.get_docs(nlp.vocab)
+    
+    all_sents = []
+    all_labels = set()
+    for doc in docs:
+        new_sent = {'tokens': [token.text for token in doc],
+                    'tags': [token.ent_iob_ + ("-" + token.ent_type_ if token.ent_type_ else '') for token in doc]}
+        all_sents.append(new_sent)
+        [all_labels.add(tag) for tag in new_sent['tags']]
+    return Dataset.from_list(all_sents), sorted(list(all_labels))
+
+
+
+def create_model_and_trainer(model_path, train, dev, all_labels, tokenizer, batch_size, epochs):
+    print("Creating model...")
+    model = AutoModelForTokenClassification.from_pretrained(model_path, num_labels=len(all_labels))
+    args = TrainingArguments(
+        f"checkpoints",
+        evaluation_strategy = "epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        gradient_accumulation_steps=3,
+        num_train_epochs=epochs,
+        weight_decay=0.01,
+        save_strategy="epoch",
+        save_total_limit=3,
+    )
+    data_collator = DataCollatorForTokenClassification(tokenizer)
+    
+    metric = load_metric("seqeval")
+
+    def compute_metrics(p):
+        predictions, labels = p
+        predictions = np.argmax(predictions, axis=2)
+
+        # Remove ignored index (special tokens)
+        true_predictions = [
+            [all_labels[p] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(predictions, labels)
+        ]
+        true_labels = [
+            [all_labels[l] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(predictions, labels)
+        ]
+
+        results = metric.compute(predictions=true_predictions, references=true_labels)
+        return {
+            "precision": results["overall_precision"],
+            "recall": results["overall_recall"],
+            "f1": results["overall_f1"],
+            "accuracy": results["overall_accuracy"],
+        }
+    
+    trainer = Trainer(
+        model,
+        args,
+        train_dataset=train,
+        eval_dataset=dev,
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics
+    )
+    return model, trainer
+
+
+def main():
+    train, labels = load_data()
+    dev, _ = load_data('training/data/dev.spacy')
+    tokenizer = AutoTokenizer.from_pretrained('nlpaueb/legal-bert-base-uncased')
+    train = process_dataset(train, tokenizer, labels)
+    dev = process_dataset(dev, tokenizer, labels)
+    model, trainer = create_model_and_trainer('./output',
+                                              train=train,
+                                              dev=dev,
+                                              all_labels=labels,
+                                              tokenizer=tokenizer,
+                                              batch_size=64,
+                                              epochs=100)
+    trainer.evaluate()
+
+if __name__ == "__main__":
+    main()
